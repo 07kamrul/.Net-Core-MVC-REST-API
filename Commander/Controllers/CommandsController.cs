899bb69e9d47ef9cc4eac71f1804e758831dafe7
kamrul <mdkamrulhasanewu@gmail.com>
2022-01-30 14:03:22 +0600

    Put or Update endpoint

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -55,5 +55,23 @@ namespace Commander.Controllers
             return CreatedAtRoute(nameof(GetCommandById), new {Id = commandReadDto.Id}, commandReadDto);
             //return Ok(commandModel);
         }
+
+        //Put api/commands/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateCommand(int id, CommandUpdateDto commandUpdateDto)
+        {
+            var commandModelFromRepo = _repository.GetCommandById(id);
+            if(commandModelFromRepo == null)
+            {
+                return NotFound();
+            }
+            _mapper.Map(commandUpdateDto, commandModelFromRepo);
+
+            _repository.UpdateCommand(commandModelFromRepo);
+
+            _repository.SaveChange();
+
+            return NoContent();
+        }
     }
 }
