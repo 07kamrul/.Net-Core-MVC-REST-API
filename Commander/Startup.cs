0917ca00301b0d97fbabd5b69059b973220c9b8e
kamrul <mdkamrulhasanewu@gmail.com>
2022-01-31 12:30:04 +0600

    Done

--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
 
 namespace Commander
 {
@@ -30,7 +31,9 @@ namespace Commander
             services.AddDbContext<CommanderContext> (opt => opt.UseSqlServer
             (Configuration.GetConnectionString("CommanderConnection")));
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(s => {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
             
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
